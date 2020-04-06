9b7ee6b44c05eefaa7bd9c4dd39c98e99546ade9
Haley Rodzos <haley.rodzos@gmail.com>
2020-04-06 02:19:57 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,26 @@
 class Shoe
+
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialize(brand)
-    @brand = brand
-  end
+  BRANDS = []
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
+  
+  def initialize(brand)
+   @brand = brand
+   BRANDS << brand
+ end
+ 
+    BRANDS = []
+    
+    def brand(brand)
+      BRANDS << brand
+      BRANDS.uniq
+    end
+  
+  #def cobble
+   # self.condition = "new"
+   # puts "Your shoe is as good as new!"
+  #end
 
 end
